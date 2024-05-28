a914beeed0fe206de034ee3c7448e732a0abcfa9
Martin Brettschneider <martin.brettschneider@adbros.cz>
2024-05-28 09:48:32 +0200

    feat: added more events

--- src/Enum/Event.php
+++ src/Enum/Event.php
@@ -5,10 +5,21 @@ namespace Adbros\Shoptet\Enum;
 enum Event: string
 {
 
+	case AddonApprove = 'addon:approve';
+	case AddonSuspend = 'addon:suspend';
+	case AddonUninstall = 'addon:uninstall';
+
 	case CustomerCreate = 'customer:create';
 	case CustomerImport = 'customer:import';
+
+	case EshopCurrencies = 'eshop:currencies';
+	case EshopBillingInformation = 'eshop:billingInformation';
+	case EshopDesign = 'eshop:design';
+	case EshopMandatoryFields = 'eshop:mandatoryFields';
+	case EshopProjectDomain = 'eshop:projectDomain';
+
 	case OrderCreate = 'order:create';
-	case OrderUpdate = 'order:update';
 	case OrderDelete = 'order:delete';
+	case OrderUpdate = 'order:update';
 
 }
